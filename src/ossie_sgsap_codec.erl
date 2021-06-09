244872373974e348773ea1195ba68178469a8f9b
Sebastian Weddmark Olsson <visnae@gmail.com>
2021-06-09 16:26:59 +0200

    cleaner decoding

--- src/ossie_sgsap_codec.erl
+++ src/ossie_sgsap_codec.erl
@@ -41,32 +41,32 @@ parse_msg_type(?SGSAP_MSGT_UE_UNREACHABLE) -> sgsap_msgt_ue_unreachable;
 parse_msg_type(_) -> sgsap_msgt_unknown.
 
 %% Chapter 8 Message functional definitions and contents
-opt_types(sgsap_msgt_alert_ack) -> [imsi];
-opt_types(sgsap_msgt_alert_reject) -> [imsi, sgs_cause];
-opt_types(sgsap_msgt_alert_request) -> [imsi];
-opt_types(sgsap_msgt_downlink_unitdata) -> [imsi, nas_message_container];
-opt_types(sgsap_msgt_eps_detach_ack) -> [imsi];
-opt_types(sgsap_msgt_eps_detach_indication) -> [imsi, mme_name, imsi_detach_from_eps_service_type];
-opt_types(sgsap_msgt_imsi_detach_ack) -> [imsi];
-opt_types(sgsap_msgt_imsi_detach_indication) -> [imsi, mme_name, imsi_detach_from_non_eps_service_type];
-opt_types(sgsap_msgt_location_update_accept) -> [imsi, location_area_identifier, mobile_identity];
-opt_types(sgsap_msgt_location_update_reject) -> [imsi, reject_cause, location_area_identifier];
-opt_types(sgsap_msgt_location_update_request) -> [imsi, mme_name, eps_location_update_type, {new_location_area_identifier, location_area_identifier}, {old_location_area_identifier, location_area_identifier}, tmsi_status, imeisv, tracking_area_identity, tracking_area_identity, tmsi_based_nri_container, selected_cs_domain_operator];
-opt_types(sgsap_msgt_mm_information_request) -> [imsi, mm_information];
-opt_types(sgsap_msgt_paging_reject) -> [imsi, sgs_cause];
-opt_types(sgsap_msgt_paging_request) -> [imsi, vlr_name, service_indicator, tmsi, cli, ss_code, lcs_indicator, lcs_client_identity, channel_needed, emlpp_priority, additional_paging_indicators, sm_delivery_timer, sm_delivery_start_time, maximum_retransmission_time];
-opt_types(sgsap_msgt_reset_ack) -> [mme_name, vlr_name];
-opt_types(sgsap_msgt_reset_indication) -> [mme_name, vlr_name];
-opt_types(sgsap_msgt_service_request) -> [imsi, service_indicator, imeisv, ue_time_zone, mobile_station_classmark_2, tracking_area_identity, e_utran_cell_global_identity, ue_emm_mode];
-opt_types(sgsap_msgt_status) -> [imsi, sgs_cause, erroneous_message];
-opt_types(sgsap_msgt_tmsi_reallocation_complete) -> [imsi];
-opt_types(sgsap_msgt_ue_activity_indication) -> [imsi, maximum_ue_availability_time];
-opt_types(sgsap_msgt_ue_unreachable) -> [imsi, sgs_cause, requested_retransmission_time, additional_ue_unreachable_indicators];
-opt_types(sgsap_msgt_uplink_unitdata) -> [imsi, nas_message_container, imeisv, ue_time_zone, mobile_station_classmark_2, tracking_area_identity, e_utran_cell_global_identity];
-opt_types(sgsap_msgt_release_request) -> [imsi, sgs_cause];
-opt_types(sgsap_msgt_service_abort_request) -> [imsi];
-opt_types(sgsap_msgt_mo_csfb_indication) -> [imsi, tracking_area_identity, e_utran_cell_global_identity];
-opt_types(sgsap_msgt_unknown) -> undefined.
+sgsap_params(sgsap_msgt_alert_ack) -> [imsi];
+sgsap_params(sgsap_msgt_alert_reject) -> [imsi, sgs_cause];
+sgsap_params(sgsap_msgt_alert_request) -> [imsi];
+sgsap_params(sgsap_msgt_downlink_unitdata) -> [imsi, nas_message_container];
+sgsap_params(sgsap_msgt_eps_detach_ack) -> [imsi];
+sgsap_params(sgsap_msgt_eps_detach_indication) -> [imsi, mme_name, imsi_detach_from_eps_service_type];
+sgsap_params(sgsap_msgt_imsi_detach_ack) -> [imsi];
+sgsap_params(sgsap_msgt_imsi_detach_indication) -> [imsi, mme_name, imsi_detach_from_non_eps_service_type];
+sgsap_params(sgsap_msgt_location_update_accept) -> [imsi, location_area_identifier, mobile_identity];
+sgsap_params(sgsap_msgt_location_update_reject) -> [imsi, reject_cause, location_area_identifier];
+sgsap_params(sgsap_msgt_location_update_request) -> [imsi, mme_name, eps_location_update_type, {new_location_area_identifier, location_area_identifier}, {old_location_area_identifier, location_area_identifier}, tmsi_status, imeisv, tracking_area_identity, tracking_area_identity, tmsi_based_nri_container, selected_cs_domain_operator];
+sgsap_params(sgsap_msgt_mm_information_request) -> [imsi, mm_information];
+sgsap_params(sgsap_msgt_paging_reject) -> [imsi, sgs_cause];
+sgsap_params(sgsap_msgt_paging_request) -> [imsi, vlr_name, service_indicator, tmsi, cli, ss_code, lcs_indicator, lcs_client_identity, channel_needed, emlpp_priority, additional_paging_indicators, sm_delivery_timer, sm_delivery_start_time, maximum_retransmission_time];
+sgsap_params(sgsap_msgt_reset_ack) -> [mme_name, vlr_name];
+sgsap_params(sgsap_msgt_reset_indication) -> [mme_name, vlr_name];
+sgsap_params(sgsap_msgt_service_request) -> [imsi, service_indicator, imeisv, ue_time_zone, mobile_station_classmark_2, tracking_area_identity, e_utran_cell_global_identity, ue_emm_mode];
+sgsap_params(sgsap_msgt_status) -> [imsi, sgs_cause, erroneous_message];
+sgsap_params(sgsap_msgt_tmsi_reallocation_complete) -> [imsi];
+sgsap_params(sgsap_msgt_ue_activity_indication) -> [imsi, maximum_ue_availability_time];
+sgsap_params(sgsap_msgt_ue_unreachable) -> [imsi, sgs_cause, requested_retransmission_time, additional_ue_unreachable_indicators];
+sgsap_params(sgsap_msgt_uplink_unitdata) -> [imsi, nas_message_container, imeisv, ue_time_zone, mobile_station_classmark_2, tracking_area_identity, e_utran_cell_global_identity];
+sgsap_params(sgsap_msgt_release_request) -> [imsi, sgs_cause];
+sgsap_params(sgsap_msgt_service_abort_request) -> [imsi];
+sgsap_params(sgsap_msgt_mo_csfb_indication) -> [imsi, tracking_area_identity, e_utran_cell_global_identity];
+sgsap_params(sgsap_msgt_unknown) -> undefined.
 
 get_num_pad_bytes(BinLenBytes) ->
     case BinLenBytes rem 2 of
@@ -84,177 +84,142 @@ parse_sgsap_opts(OptBin, OptList) when is_binary(OptBin), is_list(OptList) ->
     PadLen = get_num_pad_bytes(Length),
     LengthNet = Length - 4,
     <<CurOpt:LengthNet/binary, 0:PadLen/integer-unit:4, Remain2/binary>> = Remain,
-    NewOpt = parse_sgsap_opt(dec_iei(IEI), CurOpt);
+    NewOpt = parse_sgsap_opt(dec_iei(IEI), CurOpt),
     parse_sgsap_opts(Remain2, OptList ++ [NewOpt]).
 
 %% Chapter 9 Information element coding
-parse_sgsap_opt(sgsap_iei_cli, Bin) ->
-    <<?CLI:8, L:8, Remain/binary>> = Bin,
-    <<CurOpt:L/binary, R/binary>>,
-    {CurOpt, R};
-parse_sgsap_opt(sgsap_iei_eps_location_update_type, Bin) ->
-    <<?EPS_LOCATION_UPDATE_TYPE:8, _L:8, CurOpt:8/binary, Remain/binary>> = Bin,
-    C = case CurOpt of
-            2#00000001 -> imsi_attach;
-            2#00000010 -> normal_location_update;
+parse_sgsap_opt(sgsap_iei_cli = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_eps_location_update_type = Opt, OptBin) ->
+    C = case OptBin of
+            <<2#00000001:8>> -> imsi_attach;
+            <<2#00000010:8>> -> normal_location_update;
             _ -> normal_location_update
         end,
-    {C, Remain};
-parse_sgsap_opt(sgsap_iei_erroneous_message, Bin) ->
-    <<?ERRONEOUS_MESSAGE:8, L:8, Remain/binary>> = Bin,
-    <<CurOpt:L/binary, R/binary>> = Remain,
-    {CurOpt, R};
-parse_sgsap_opt(sgsap_iei_e_utran_cell_global_identity, Bin) ->
-    <<?E_UTRAN_CELL_GLOBAL_IDENTITY:8, L:8, Remain/binary>> = Bin,
-    <<CurOpt:L/binary, R/binary>> = Remain,
-    {CurOpt, R};
-parse_sgsap_opt(sgsap_iei_global_cn_id, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_imeisv, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_imsi, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_imsi_detach_from_eps_service_type, Bin) ->
-    <<?IMSI_DETACH_FROM_EPS_SERVICE_TYPE:8, _L:8, CurOpt:8/binary, Remain/binary>> = Bin,
-    C = case CurOpt of
-            2#00000001 -> network_initiated_imsi_detach_from_eps_services;
-            2#00000010 -> ue_initiated_imsi_detach_from_eps_services;
-            2#00000011 -> eps_services_not_allowed;
-            _ -> {reserved, CurOpt}
+    {Opt, C};
+parse_sgsap_opt(sgsap_iei_erroneous_message = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_e_utran_cell_global_identity = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_global_cn_id = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_imeisv = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_imsi = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_imsi_detach_from_eps_service_type = Opt, OptBin) ->
+    C = case OptBin of
+            <<2#00000001:8>> -> network_initiated_imsi_detach_from_eps_services;
+            <<2#00000010:8>> -> ue_initiated_imsi_detach_from_eps_services;
+            <<2#00000011:8>> -> eps_services_not_allowed;
+            _ -> {reserved, OptBin}
         end,
-    {C, Remain};
-parse_sgsap_opt(sgsap_iei_imsi_detach_from_non_eps_service_type, Bin) ->
-    <<?IMSI_DETACH_FROM_NON_EPS_SERVICE_TYPE:8, _L:8, CurOpt:8/binary, Remain/binary>> = Bin,
-    C = case CurOpt of
-            2#00000001 -> explicit_ue_initiated_imsi_detach_from_non_eps_services;
-            2#00000010 -> combined_ue_initiated_imsi_detach_from_eps_and_non_eps_services;
-            2#00000011 -> implicit_network_initiated_imsi_detach_from_eps_and_non_eps_services;
-            _ -> {reserved, CurOpt}
+    {Opt, C};
+parse_sgsap_opt(sgsap_iei_imsi_detach_from_non_eps_service_type = Opt, OptBin) ->
+    C = case OptBin of
+            <<2#00000001:8>> -> explicit_ue_initiated_imsi_detach_from_non_eps_services;
+            <<2#00000010:8>> -> combined_ue_initiated_imsi_detach_from_eps_and_non_eps_services;
+            <<2#00000011:8>> -> implicit_network_initiated_imsi_detach_from_eps_and_non_eps_services;
+            _ -> {reserved, OptBin}
         end,
-    {C, Remain};
-parse_sgsap_opt(sgsap_iei_lcs_client_identity, Bin) ->
-    <<?LCS_CLIENT_IDENTITY:8, L:8, Remain/binary>> = Bin,
-    <<CurOpt:L/binary, R/binary>> = Remain,
-    {CurOpt, R};
-parse_sgsap_opt(sgsap_iei_lcs_indicator, Bin) ->
-    <<?LCS_INDICATOR:8, _L:8, CurOpt:8/binary, Remain/binary>> = Bin,
-    C = case CurOpt of
-            2#00000001 -> mt_lr;
-            _ -> {unspecified, CurOpt}
+    {Opt, C};
+parse_sgsap_opt(sgsap_iei_lcs_client_identity = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_lcs_indicator = Opt, OptBin) ->
+    C = case OptBin of
+            <<2#00000001:8>> -> mt_lr;
+            _ -> {unspecified, OptBin}
         end,
-    {C, Remain};
-parse_sgsap_opt(sgsap_iei_location_area_identifier, Bin) ->
-    <<?LOCATION_AREA_IDENTIFIER:8, L:8, Remain/binary>> = Bin,
-    <<CurOpt:L/binary, R/binary>> = Remain,
-    {CurOpt, R};
-parse_sgsap_opt(sgsap_iei_mm_information, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_mme_name, Bin) ->
-    <<?MME_NAME:8, L:8, Remain/binary>> = Bin,
-    <<CharBin:((57-2)*8)/binary, R/binary>> = Remain,
-    Chars = [C || <<_L:8, C:8, _:8>> <= CharBin],
-    {Chars, R};
-parse_sgsap_opt(sgsap_iei_mobile_identity, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_mobile_station_classmark_2, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_nas_message_container, Bin) ->
-    <<?NAS_MESSAGE_CONTAINER:8, L:8, Remain/binary>> = Bin,
-    <<CurOpt:L/binary, R/binary>> = Remain,
-    {CurOpt, R};
-parse_sgsap_opt(sgsap_iei_reject_cause, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_service_indicator, Bin) ->
-    <<?SERVICE_INDICATOR:8, _L:8, CurOpt:8/binary, Remain/binary>> = Bin,
-    <<CurOpt:8, R/binary>>,
-    C = case CurOpt of
-            2#00000001 -> cs_call_indicator;
-            2#00000010 -> sms_indicator;
+    {Opt, C};
+parse_sgsap_opt(sgsap_iei_location_area_identifier = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_mm_information = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_mme_name = Opt, OptBin) ->
+    Chars = [C || <<_L:8, C:8, _:8>> <= OptBin],
+    {Opt, Chars};
+parse_sgsap_opt(sgsap_iei_mobile_identity = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_mobile_station_classmark_2 = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_nas_message_container = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_reject_cause = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_service_indicator = Opt, OptBin) ->
+    C = case OptBin of
+            <<2#00000001:8>> -> cs_call_indicator;
+            <<2#00000010:8>> -> sms_indicator;
             _ -> cs_call_indicator
         end,
-    {C, R};
-parse_sgsap_opt(sgsap_iei_sgs_cause, Bin) ->
-    <<?SGS_CAUSE:8, _L:8, CurOpt:8/binary, Remain/binary>> = Bin,
-    C = case CurOpt of
-            2#00000001 -> imsi_detached_for_eps_services;
-            2#00000010 -> imsi_detached_for_eps_and_non_eps_services;
-            2#00000011 -> imsi_unknown;
-            2#00000100 -> imsi_detached_for_non_eps_services;
-            2#00000101 -> imsi_implicitly_detached_for_non_eps_services;
-            2#00000110 -> ue_unreachable;
-            2#00000111 -> message_not_compatible_with_the_protocol_state;
-            2#00001000 -> missing_mandatory_information_element;
-            2#00001001 -> invalid_mandatory_information;
-            2#00001010 -> conditional_information_element_error;
-            2#00001011 -> semantically_incorrect_message;
-            2#00001100 -> message_unknown;
-            2#00001101 -> mobile_terminating_cs_fallback_call_rejected_by_the_user;
-            2#00001110 -> ue_temporarily_unreachable;
-            _ -> {unspecified, CurOpt}
+    {Opt, C};
+parse_sgsap_opt(sgsap_iei_sgs_cause = Opt, OptBin) ->
+    C = case OptBin of
+            <<2#00000001:8>> -> imsi_detached_for_eps_services;
+            <<2#00000010:8>> -> imsi_detached_for_eps_and_non_eps_services;
+            <<2#00000011:8>> -> imsi_unknown;
+            <<2#00000100:8>> -> imsi_detached_for_non_eps_services;
+            <<2#00000101:8>> -> imsi_implicitly_detached_for_non_eps_services;
+            <<2#00000110:8>> -> ue_unreachable;
+            <<2#00000111:8>> -> message_not_compatible_with_the_protocol_state;
+            <<2#00001000:8>> -> missing_mandatory_information_element;
+            <<2#00001001:8>> -> invalid_mandatory_information;
+            <<2#00001010:8>> -> conditional_information_element_error;
+            <<2#00001011:8>> -> semantically_incorrect_message;
+            <<2#00001100:8>> -> message_unknown;
+            <<2#00001101:8>> -> mobile_terminating_cs_fallback_call_rejected_by_the_user;
+            <<2#00001110:8>> -> ue_temporarily_unreachable;
+            _ -> {unspecified, OptBin}
         end,
-    {C, Remain};
-parse_sgsap_opt(sgsap_iei_ss_code, Bin) ->
-    <<?SS_CODE:8, _L:8, CurOpt:8/binary, Remain/binary>> = Bin,
-    {CurOpt, Remain};
-parse_sgsap_opt(sgsap_iei_tmsi, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_tmsi_status, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_tracking_area_identity) ->
-    <<?TRACKING_AREA_IDENTITY:8, L:8, Remain/binary>> = Bin,
-    <<CurOpt:L/binary, R/binary>> = Remain,
-    {CurOpt, R};
-parse_sgsap_opt(sgsap_iei_ue_time_zone, Bin) ->
-    <<?UE_TIME_ZONE:8, _L:8, CurOpt:8/binary, Remain/binary>> = Bin,
-    {CurOpt, Remain};
-parse_sgsap_opt(sgsap_iei_ie_emm_mode) ->
-    <<?UE_EMM_MODE:8, _L:8, CurOpt:8/binary, Remain/binary>> = Bin,
-    C = case CurOpt of
-            2#00000001 -> emm_idle;
-            2#00000010 -> emm_connected;
-            _ -> {reserved, CurOpt}
+    {Opt, C};
+parse_sgsap_opt(sgsap_iei_ss_code = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_tmsi = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_tmsi_status = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_tracking_area_identity = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_ue_time_zone = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_ie_emm_mode = Opt, OptBin) ->
+    C = case OptBin of
+            <<2#00000001:8>> -> emm_idle;
+            <<2#00000010:8>> -> emm_connected;
+            _ -> {reserved, OptBin}
         end,
-    {C, Remain};
-parse_sgsap_opt(sgsap_iei_vlr_name, Bin) ->
-    <<?VLR_NAME:8, L:8, Remain/binary>> = Bin,
-    <<CharBin:L/binary, R/binary>> = Remain,
-    Chars = [C || <<_L:8, C:8, _:8>> <= CharBin],
-    {Chars, R};
-parse_sgsap_opt(sgsap_iei_channel_needed, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_emlpp_priority, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_additional_paging_indicators, Bin) ->
-    <<?ADDITIONAL_PAGING_INDICATORS:8, _L:8, 0:7, CurOpt:1/binary, Remain/binary>> = Bin,
-    C = case CurOpt of
-            2#1 -> cs_restore_indicator_not_set;
-            2#0 -> cs_restore_indicator_set
+    {Opt, C};
+parse_sgsap_opt(sgsap_iei_vlr_name = Opt, OptBin) ->
+    Chars = [C || <<_L:8, C:8, _:8>> <= OptBin],
+    {Opt, Chars};
+parse_sgsap_opt(sgsap_iei_channel_needed = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_emlpp_priority = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_additional_paging_indicators = Opt, OptBin) ->
+    C = case OptBin of
+            <<0:7, 2#1:1>> -> cs_restore_indicator_not_set;
+            <<0:7, 2#0:1>> -> cs_restore_indicator_set
         end,
-    {C, Remain};
-parse_sgsap_opt(sgsap_iei_tmsi_based_nri_container, Bin) ->
-    ;
-parse_sgsap_opt(sgsap_iei_selected_cs_domain_operator, Bin) ->
-    <<?SELECTED_CS_DOMAIN_OPERATOR:8, _L:8, CurOpt:24/binary, Remain/binary>> = Bin,
-    {CurOpt, Remain};
-parse_sgsap_opt(sgsap_iei_maximum_ue_availability_time, Bin) ->
-    <<?MAXIMUM_UE_AVAILABILITY_TIME:8, _L:8, CurOpt:32/binary, Remain/binary>> = Bin,
-    {CurOpt, Remain};
-parse_sgsap_opt(sgsap_iei_sm_delivery_start_time, Bin) ->
-    <<?SM_DELIVERY_START_TIME:8, _L:8, CurOpt:32/binary, Remain/binary>> = Bin,
-    {CurOpt, Remain};
-parse_sgsap_opt(sgsap_iei_additional_ue_unreachable_indicators, Bin) ->
-    <<?ADDITIONAL_UE_UNREACHABLE_INDICATORS:8, _L:8, 0:7, CurOpt:1/binary, Remain/binary>> = Bin,
-    C = case CurOpt of
-            2#1 -> sm_buffer_request_indicator_not_set;
-            2#0 -> sm_buffer_request_indicator_set
+    {Opt, C};
+parse_sgsap_opt(sgsap_iei_tmsi_based_nri_container = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_selected_cs_domain_operator = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_maximum_ue_availability_time = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_sm_delivery_start_time = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_additional_ue_unreachable_indicators = Opt, OptBin) ->
+    C = case OptBin of
+            <<0:7, 2#1:1>> -> sm_buffer_request_indicator_not_set;
+            <<0:7, 2#0:1>> -> sm_buffer_request_indicator_set
         end,
-    {C, Remain};
-parse_sgsap_opt(sgsap_iei_maximum_retransmission_time, Bin) ->
-    <<?MAXIMUM_RETRANSMISSION_TIME:8, _L:8, CurOpt:32/binary, Remain/binary>> = Bin,
-    {CurOpt, Remain};
-parse_sgsap_opt(sgsap_iei_requested_retransmission_time, Bin) ->
-    <<?REQUESTED_RETRANSMISSION_TIME:8, _L:8, CurOpt:32/binary, Remain/binary>> = Bin,
-    {CurOpt, Remain};
+    {Opt, C};
+parse_sgsap_opt(sgsap_iei_maximum_retransmission_time = Opt, OptBin) ->
+    {Opt, OptBin};
+parse_sgsap_opt(sgsap_iei_requested_retransmission_time = Opt, OptBin) ->
+    {Opt, OptBin}.
 
 dec_iei(?SGSAP_IEI_IMSI) -> sgsap_iei_imsi;
 dec_iei(?SGSAP_IEI_VLR_NAME) -> sgsap_iei_vlr_name;
