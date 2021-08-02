695a474337a55c92161c89ff8b451536509bb61a
Sebastian Weddmark Olsson <visnae@gmail.com>
2021-08-02 11:09:33 +0200

    cleanup

--- src/ossie_util.erl
+++ src/ossie_util.erl
@@ -241,7 +241,8 @@ dec_tbcd_digit(2#1010) -> $*;
 dec_tbcd_digit(2#1011) -> $#;
 dec_tbcd_digit(2#1100) -> $a;
 dec_tbcd_digit(2#1101) -> $b;
-dec_tbcd_digit(2#1110) -> $c.
+dec_tbcd_digit(2#1110) -> $c;
+dec_tbcd_digit(2#1111) -> $f.
 
 encode_tbcd(Str) ->
     encode_tbcd(Str, <<>>).
@@ -249,7 +250,7 @@ encode_tbcd(Str) ->
 encode_tbcd([D1, D2 | Ds], Acc) ->
     encode_tbcd(Ds, enc_tbcd_digits(D1, D2, Acc));
 encode_tbcd([D], Acc) ->
-    enc_tbcd_digits(D, pad, Acc);
+    enc_tbcd_digits(D, $f, Acc);
 encode_tbcd([], Acc) ->
     Acc.
 
@@ -257,12 +258,12 @@ enc_tbcd_digits(D1, D2, Acc) ->
     <<Acc/binary, (enc_tbcd_digit(D2)):4, (enc_tbcd_digit(D1)):4>>.
 
 enc_tbcd_digit(D) when D >= $0, D =< $9   -> D - $0;
-enc_tbcd_digit(pad)                       -> 2#1111;
 enc_tbcd_digit($*)                        -> 2#1010;
 enc_tbcd_digit($#)                        -> 2#1011;
 enc_tbcd_digit(A) when A =:= $A; A =:= $a -> 2#1100;
 enc_tbcd_digit(B) when B =:= $B; B =:= $b -> 2#1101;
-enc_tbcd_digit(C) when C =:= $C; C =:= $c -> 2#1110.
+enc_tbcd_digit(C) when C =:= $C; C =:= $c -> 2#1110;
+enc_tbcd_digit(F) when F =:= $F; F =:= $f -> 2#1111.
 
 decode_mcc_mnc(<<MCC2:4, MCC1:4, 2#1111:4, MCC3:4, MNC2:4, MNC1:4>>) ->
     lists:map(fun dec_tbcd_digit/1, [MCC1, MCC2, MCC3, MNC1, MNC2]);
